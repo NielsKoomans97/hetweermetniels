a3c21143ac973439a1efd16237febca46b0e3aef
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2023-08-30 11:01:13 +0200

    added remove location button
    - added button at every saved location to remove location

--- scripts/locations.js
+++ scripts/locations.js
@@ -25,19 +25,34 @@ function SaveLocation(item) {
     SaveCookie(new Cookie('locations', JSON.stringify(database)));
 }
 
-function RemoveAllLocations(){
-    if (HasCookie('locations')){
-        SaveCookie(new Cookie('locations',''));
+function RemoveAllLocations() {
+    if (HasCookie('locations')) {
+        SaveCookie(new Cookie('locations', ''));
     }
 }
 
-function Search(query){   
+function RemoveLocation(item) {
+    const data = JSON.parse(item.getAttribute('data-json'));
+    const locations = ListLocations();
+
+    for (let i = 0; i < locations.length; i++) {
+        const location = JSON.parse(locations[i]);
+
+        if (location['name'] == data['name']) {
+            locations.splice(i, i);
+        }
+    }
+
+    SaveCookie(new Cookie('locations', JSON.stringify(locations)));
+}
+
+function Search(query) {
     let finalResult = fetch(`https://location.buienradar.nl/1.1/location/search?query=${query}`)
         .then((response) => (response.json()))
         .then((data) => {
             let results = [];
 
-            for(let result of data){
+            for (let result of data) {
                 results.push(result);
             }
 
@@ -47,7 +62,7 @@ function Search(query){
     return finalResult;
 }
 
-async function SaveDefaultLocation(){
+async function SaveDefaultLocation() {
     let results = await Search('de bilt');
     let de_bilt = JSON.stringify(results[0]);
     SaveLocation(de_bilt);
--- scripts/settings.js
+++ scripts/settings.js
@@ -19,19 +19,35 @@ locationItems.forEach(function (savedItem) {
 function CreateSavedLocationItem(item, root) {
     let parsed = JSON.parse(item);
     let savedLocation = document.createElement('div');
-    savedLocation.className = 'saved-location';
+    savedLocation.classList.add('saved-location', 'd-flex');
+
+    let locationInfoContainer = document.createElement('div');
+    locationInfoContainer.classList.add('col', 'd-flex', 'flex-column');
 
     let locationName = document.createElement('h6');
     locationName.innerText = parsed['name'];
     locationName.className = 'location-name';
-    savedLocation.appendChild(locationName);
+    locationInfoContainer.appendChild(locationName);
 
     if (parsed['foad'] != null) {
         let locationFoad = document.createElement('p');
         locationFoad.innerText = `${parsed['foad']['name']}, ${parsed['country']}`;
         locationFoad.className = 'location-foad';
-        savedLocation.appendChild(locationFoad);
+        locationInfoContainer.appendChild(locationFoad);
     }
+
+    savedLocation.appendChild(locationInfoContainer);
+
+    let removeLocationButton = document.createElement('button');
+    removeLocationButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    removeLocationButton.id = 'remove-location';
+    removeLocationButton.setAttribute('data-json', item);
+    removeLocationButton.addEventListener('click', () => {
+        console.log('ik doe wat hahaha');
+        RemoveLocation(removeLocationButton);
+    });
+
+    savedLocation.appendChild(removeLocationButton);
 
     root.appendChild(savedLocation);
 }
@@ -62,12 +78,12 @@ function CreateSearchResult(data, root) {
     if (data['foad'] != null) {
         country = `${data['foad']['name']}, `;
     }
-    
+
     if (data['country'] != null) {
         country += `${data['country']}`;
     }
 
-    if (country != ''){
+    if (country != '') {
         locationFoad.innerText = country;
     }
     else {
