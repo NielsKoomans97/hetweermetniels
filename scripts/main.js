d1673811f83df6b1ed66e7c3cb254723e17da367
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2023-10-01 21:53:57 +0200

    updates
    - kind of finished search

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,10 @@
 import { Announcements } from "./announcements.js";
-import { CookieManager } from "./cookies.js";
 import { Locations } from "./locations.js";
 import { MenuExpander } from "./nav-menu.js";
 import { ObservationsWidget } from "./observations.js";
 import { SmallerRadar } from "./radar-small.js";
 
 new MenuExpander();
-var cookieManager = new CookieManager();
-
 if (HasElement('observations-widget') == true) {
     const widget = new ObservationsWidget(6340);
     await widget.Refresh();
