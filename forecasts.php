2e18b90a658bfb13558f7c9e5f256ffc08bfb564
Niels <niels.koomans@gmail.com>
2023-10-03 14:18:27 +0200

    updates
    - van alles

--- forecasts.php
+++ forecasts.php
@@ -3,9 +3,17 @@
 
     <section id="daily-forecast">
         <div class="container">
-            <h4>Voorspellingen</h4>
-            <div class="col" id="forecast-days">
-               
+            <div class="col" id="forecast-expander-bar">
+                <h4>Voorspellingen</h4>
+                <button class="nav-button" id="forecast-expand"><i class="fas fa-arrow-down"></i></button>
+            </div>
+            <div class="col">
+                <div class="col" id="forecast-days" data-collapsed="collapsed">
+
+                </div>
+                <div class="col" id="forecast-scroller">
+                    
+                </div>
             </div>
         </div>
     </section>
