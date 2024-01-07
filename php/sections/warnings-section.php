5e8cd5345f68f6511f4e5cbc11e5300895d217f1
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-01-08 00:29:33 +0100

    fresh start

--- php/sections/warnings-section.php
+++
@@ -1,8 +0,0 @@
-<section id="warnings">
-    <div class="container d-flex flex-column flex-grow">
-        <h3><b>Waarschuwing(en) per locatie</b></h3>
-        <div class="col grid" id="warnings-list">
-
-        </div>
-    </div>
-</section>
