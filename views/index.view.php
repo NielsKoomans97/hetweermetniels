a8b958e9be5acab6e0a37482a515a782d3340e80
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-01-10 23:47:45 +0100

    updates
    - more styling

--- views/index.view.php
+++ views/index.view.php
@@ -1,3 +1,15 @@
-<img id="current-radar" src="https://processing-cdn.buienradar.nl/processing/nl/rain48hour/forecast/runs/webm/202401091300/202401100400.png">
+<div class="map-grid">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+    <img class="map-tile">
+</div>
+<img id="current-radar">
 
 <?= component('chartslider'); ?>
