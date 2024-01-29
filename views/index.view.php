68eb6571a5f2e9f21aca0cb16a972c9cf3f35b8b
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-01-29 12:19:45 +0100

    updates
    - started working on homepage

---
+++ views/index.view.php
@@ -0,0 +1,16 @@
+<section id="hero">
+    <img class="banner" src="data/Static/weather_JannekeGuenther.jpg">
+    <div class="container">
+        <div class="more-info">
+            <p>Eerst deed ik altijd één plak ham op mijn boterham, nu twee</p>
+            <h1>Vind uit wat voor gevolgen dit heeft voor <u>UW LEVEN</u>.</h1>
+            <div class="row">
+                <a href="/about" class="primary-button">Over</a>
+                <a href="/stiching-sterf" class="secondary-button">Ik ga liever nu dood</a>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
