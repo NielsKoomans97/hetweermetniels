db414bccb8fcbac7c06ed59482f7fd853bf6663c
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-02-14 16:45:36 +0100

    updates
    - iets meer functionele 404, maar nog steeds niet goed genoeg

--- views/404.view.php
+++ views/404.view.php
@@ -3,7 +3,7 @@
         <div class="col-md-6">
             <i class="bi bi-exclamation-lg"></i>
             <h1><b>404</b></h1>
-            <h2>Niet gevonden</h2>
+            <h2>Niet gevonden. <a href="/">Terug naar home</a></h2>
         </div>
     </div>
 </section>
